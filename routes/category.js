0f7bdc23c144c03f9defa455a8ae9e6ec6786ae0
Buba Mohammed Auwal <juniorbuba4real@gmail.com>
2022-02-23 13:14:49 +0100

    fix: truncated redundant code

--- routes/category.js
+++ routes/category.js
@@ -19,47 +19,4 @@ router.get('/categories', async function (req, res) {
     }
 })
 
-router.get('/product/:id', async (req, res) => {
-    const _id = req.params.id
-    try{
-        const product = await Product.findAll({
-            where: {
-                product_name: _id
-            }
-        })
-        if(product.length === 0){
-            res.status(404).send({null: 'no products found for given ID'})
-        }
-        else{
-            res.status(200).send({product})
-        }
-    }
-    catch(e){
-        res.status(500).send({fail: 'could not fetch single product details'})
-    }
-})
-
-router.get('/product/search/:id', async (req, res) => {
-    const _id = req.params.id
-    try{
-        const product = await Product.findAll({
-            where: {
-                name: {
-                    // [Op.startsWith]: `${_id}`,
-                    [Op.like]: `%${_id}%`
-                }
-            }
-        })
-        if(product.length === 0){
-            res.status(404).send({null: 'Product does not exist'})
-        }
-        else{
-            res.status(200).send({product})
-        }
-    }
-    catch(e){
-        res.status(500).send({fail: 'Could not fetch product details'})
-    }
-})
-
 module.exports = router
