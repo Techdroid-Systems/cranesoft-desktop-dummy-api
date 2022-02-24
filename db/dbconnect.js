750db76e01c0313497197bf828921032d97e481d
Buba Mohammed Auwal <juniorbuba4real@gmail.com>
2022-02-24 15:50:09 +0100

    test: wrong password on heroku

--- db/dbconnect.js
+++ db/dbconnect.js
@@ -8,7 +8,7 @@ const Customer = require('../models/customer')
 var connection = {
     host     : '209.205.200.218',
     user     : 'modelout_modelout',
-    password : 'fvDjq)CA;&Oa',
+    password : 'fvDjq)CA;&O',
     port: 3306,
     database : 'modelout_cranesoft_cloud_db'
   };
